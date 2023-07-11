1d7fb8fbe8e9d3724069d3ef04ff8cf4dfddf1f7
RahulYerme <81137674+RahulYerme@users.noreply.github.com>
2023-07-12 00:06:50 +0530

    Update SpringBootDataJpaApplication.java

--- spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,6 +2,12 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
