3d013a72ba5f383f269d618f250dd7bc4b0c3ca7
RahulYerme <81137674+RahulYerme@users.noreply.github.com>
2023-07-12 00:03:49 +0530

    Update SpringBootDataJpaApplication.java

--- spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDataJpaApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/*").allowedOrigins("http://localhost:9090");
+		registry.addMapping("/*").allowedOrigins("http://127.0.0.1:8080");
 			}
 			
 		};
